1f722a2aaba147ada651c2dbb3150489c2432baa
EasyAppClaude <d12299888@gmail.com>
2026-06-28 16:11:07 +0200

    Add files via upload

---
+++ vyshnia-kasa 3/sw.js
@@ -0,0 +1,34 @@
+const CACHE = "restpos-test-v24";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./icons.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+// Network-first: always try the network, fall back to cache only when offline.
+self.addEventListener("fetch", e => {
+  const url = new URL(e.request.url);
+  if (url.origin !== location.origin) return;
+  e.respondWith(
+    fetch(e.request).then(r => {
+      if (r.ok) {
+        const copy = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+      }
+      return r;
+    }).catch(() => caches.match(e.request))
+  );
+});
